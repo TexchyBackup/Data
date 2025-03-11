32bbbffc031fd11cd5ce03744ceffd0b66ad7472
TexchyBackup <pancececka@gmail.com>
2025-03-11 17:44:38 +0100

    Create Materials.lua

---
+++ Materials.lua
@@ -0,0 +1,60 @@
+return {
+    ["Copper Sword"] = {
+        Materials = {
+            ["Copper Bar"] = 4,
+            ["Light Leather"] = 2
+        }
+    },
+    ["Copper Axe"] = {
+        Materials = {
+            ["Copper Bar"] = 2,
+            ["Oak Log"] = 1
+        }
+    },
+    ["Copper Sickle"] = {
+        Materials = {
+            ["Copper Bar"] = 2,
+            ["Oak Log"] = 1
+        }
+    },
+    ["Copper Pickaxe"] = {
+        Materials = {
+            ["Copper Bar"] = 2,
+            ["Oak Log"] = 1
+        }
+    },
+    ["Copper Greatsword"] = {
+        Materials = {
+            ["Copper Bar"] = 4,
+            ["Light Leather"] = 2
+        }
+    },
+    ["Copper Dagger"] = {
+        Materials = {
+            ["Copper Bar"] = 4,
+            ["Light Leather"] = 2
+        }
+    },
+    ["Copper Shield"] = {
+        Materials = {
+            ["Copper Bar"] = 4,
+            ["Light Leather"] = 2
+        }
+    },
+    ["Copper Chestplate"] = {
+        Materials = {
+            ["Copper Bar"] = 4
+        }
+    },
+    ["Copper Boots"] = {
+        Materials = {
+            ["Copper Bar"] = 2
+        }
+    },
+    ["Copper Helmet"] = {
+        Materials = {
+            ["Copper Bar"] = 2
+        }
+    }
+}
+
